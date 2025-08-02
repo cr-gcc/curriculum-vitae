8944f73898e7a2ca6b5a136d3f9d8c7669ee9b46
Cris GC <cris.gcc@outlook.com>
2025-08-02 14:11:39 -0600

    Actualizacion

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -18,174 +18,181 @@
     <div class="mt-1 flex flex-col md:flex-row text-justify">
         <!-- Columna izquierda -->
         <div class="w-full md:w-3/5 p-4 border-r-2 border-gray-400">
-            <section class="mb-3">
-                <h2 class="text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">Perfil Profesional</h2>
-                <p class="leading-tight mt-2 text-sm">
-                    Ingeniero en computación con amplia experiencia en diferentes sectores (educativo, bancario, construcción, logística etc). Enfocado en aportar valor tanto en proyectos individuales como en equipo. Abierto al cambio, con constante disposición para aprender y adaptarse a nuevas tecnologías y habilidades.
-                </p>
-            </section>
-            <section>
-                <h2 class="mb-2 text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">
-                    Experiencia laboral
-                </h2>
-                <div class="mb-1">
-                    <h3 class="font-bold leading-tight">
-                        Big Move Smart
-                        <span class="text-sm font-normal">| Enero 2022 - Marzo 2025</span>
-                    </h3>
-                    <p class="text-sm leading-tight">
-                        <span class="font-semibold">Desarrollador Back-End:</span>
-                        Soporte al equipo mobile. Integración con APIs externas y propias. Mejora y automatización de procesos logísticos para delivery.
-                    </p>
-                </div>
-                <div class="mb-1">
-                    <h3 class="font-bold leading-tight">
-                        Laureate International Universities
-                        <span class="text-sm font-normal">| Feb 2018 - Dic 2021</span>
-                    </h3>
-                    <p class="text-sm leading-tight">
-                        <span class="font-semibold">Programador Full Stack:</span>
-                        Encargado del ciclo de vida de los sistemas propios, para la optimización de procesos corporativos. Usando Azure AD para la gestión de acceso.
-                    </p>
-                </div>
-                <div class="mb-1">
-                    <h3 class="font-bold leading-tight">
-                        Envíos Pronto - Freelancer
-                        <span class="text-sm font-normal">| Mar 2021 - Ene 2022</span>
-                    </h3>
-                    <p class="text-sm leading-tight">
-                        <span class="font-semibold">Desarrollador Full Stack:</span>
-                        Análisis, implementación y mantenimiento de módulos del sistema, tanto en frontend, backend como base de datos.
-                    </p>
-                </div>
-                <div class="mb-1">
-                    <h3 class="font-bold leading-tight">
-                        UNAM COUS - Freelancer
-                        <span class="text-sm font-normal">| Ago 2020 - Nov 2021</span>
-                    </h3>
-                    <p class="text-sm leading-tight">
-                        <span class="font-semibold">Desarrollador Web:</span>
-                        Líder del área de desarrollo, encargado de la divulgación de proyectos sustentables mediante el desarrollo de sistemas y páginas web.
-                    </p>
-                </div>
-                <div class="mb-1">
-                    <h3 class="font-bold leading-tight">
-                        UNAM FCA CEDIGEC - Freelancer
-                        <span class="text-sm font-normal">| Sep 2020 - Dic 2025</span>
-                    </h3>
-                    <p class="text-sm leading-tight">
-                        <span class="font-semibold">Desarrollador Web:</span>
-                        Soporte y desarrollo de programas universitarios y gubernamentales. Creación de sistemas y sitios web para procesos internos y administración de diplomados en línea mediante Moodle.
-                    </p>
-                </div>
-                <div class="mb-1">
-                    <h3 class="font-bold leading-tight">
-                        Coordina Grupo Sagma - Freelancer
-                        <span class="text-sm font-normal">| Ago 2019 - Nov 2019</span>
-                    </h3>
-                    <p class="text-sm leading-tight">
-                        <span class="font-semibold">Programador Full Stack:</span>
-                        Análisis y desarrollo de sistemas de auditoría y control interno, para la evaluación y visibilidad de avances de construcción en tramos carreteros en colaboración con CAPUFE.
-                    </p>
-                </div>
-                <div class="mb-1">
-                    <h3 class="font-bold leading-tight">
-                        Banco de México
-                        <span class="text-sm font-normal">| Oct 2017 - Ene 2018</span>
-                    </h3>
-                    <p class="text-sm leading-tight">
-                        <span class="font-semibold">Analista de información:</span>
-                        Análisis y soporte a las diferentes entidades bancarias. Revisión de flujos de información de transacciones.
-                    </p>
-                </div>
-                <div class="mb-1">
-                    <h3 class="font-bold leading-tight">
-                        CNBV - Freelancer
-                        <span class="text-sm font-normal">| Jul 2016 - Sep 2016</span>
-                    </h3>
-                    <p class="text-sm leading-tight">
-                        <span class="font-semibold">Desarrollador Web:</span>
-                        Análisis de datos para la elaboración de reportes, mediante el desarrollo de sistemas propietarios.
-                    </p>
-                </div>
-                <div>
-                    <h3 class="font-bold leading-tight">
-                        Unisys de México, S.A. de C.V.
-                        <span class="text-sm font-normal">| Dic 2013 - Dic 2015</span>
-                    </h3>
-                    <p class="text-sm leading-tight">
-                        <span class="font-semibold">Student Technical:</span>
-                        Monitoreo de servidores y sistemas basados en COBOL, así como el desarrollo y prueba de nuevas tecnologías.
-                    </p>
-                </div>
-            </section>
+          <section class="mb-3">
+              <h2 class="text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">Perfil Profesional</h2>
+              <p class="leading-tight mt-2 text-sm">
+                  Ingeniero en computación con amplia experiencia en diferentes sectores (educativo, bancario, construcción, logística etc). Enfocado en aportar valor tanto en proyectos individuales como en equipo. Abierto al cambio, con constante disposición para aprender y adaptarse a nuevas tecnologías y habilidades.
+              </p>
+          </section>
+          <section>
+              <h2 class="mb-2 text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">
+                  Experiencia laboral
+              </h2>
+              <div class="mb-1">
+                  <h3 class="font-bold leading-tight">
+                      Big Move Smart
+                      <span class="text-sm font-normal">| Enero 2022 - Marzo 2025</span>
+                  </h3>
+                  <p class="text-sm leading-tight">
+                      <span class="font-semibold">Desarrollador Back-End:</span>
+                      Soporte al equipo mobile. Integración con APIs externas y propias. Mejora y automatización de procesos logísticos para delivery.
+                  </p>
+              </div>
+              <div class="mb-1">
+                  <h3 class="font-bold leading-tight">
+                      Laureate International Universities
+                      <span class="text-sm font-normal">| Feb 2018 - Dic 2021</span>
+                  </h3>
+                  <p class="text-sm leading-tight">
+                      <span class="font-semibold">Programador Full Stack:</span>
+                      Encargado del ciclo de vida de los sistemas propios, para la optimización de procesos corporativos. Usando Azure AD para la gestión de acceso.
+                  </p>
+              </div>
+              <div class="mb-1">
+                  <h3 class="font-bold leading-tight">
+                      Envíos Pronto - Freelancer
+                      <span class="text-sm font-normal">| Mar 2021 - Ene 2022</span>
+                  </h3>
+                  <p class="text-sm leading-tight">
+                      <span class="font-semibold">Desarrollador Full Stack:</span>
+                      Análisis, implementación y mantenimiento de módulos del sistema, tanto en frontend, backend como base de datos.
+                  </p>
+              </div>
+              <div class="mb-1">
+                  <h3 class="font-bold leading-tight">
+                      UNAM COUS - Freelancer
+                      <span class="text-sm font-normal">| Ago 2020 - Nov 2021</span>
+                  </h3>
+                  <p class="text-sm leading-tight">
+                      <span class="font-semibold">Desarrollador Web:</span>
+                      Líder del área de desarrollo, encargado de la divulgación de proyectos sustentables mediante el desarrollo de sistemas y páginas web.
+                  </p>
+              </div>
+              <div class="mb-1">
+                  <h3 class="font-bold leading-tight">
+                      UNAM FCA CEDIGEC - Freelancer
+                      <span class="text-sm font-normal">| Sep 2020 - Dic 2025</span>
+                  </h3>
+                  <p class="text-sm leading-tight">
+                      <span class="font-semibold">Desarrollador Web:</span>
+                      Soporte y desarrollo de programas universitarios y gubernamentales. Creación de sistemas y sitios web para procesos internos y administración de diplomados en línea mediante Moodle.
+                  </p>
+              </div>
+              <div class="mb-1">
+                  <h3 class="font-bold leading-tight">
+                      Coordina Grupo Sagma - Freelancer
+                      <span class="text-sm font-normal">| Ago 2019 - Nov 2019</span>
+                  </h3>
+                  <p class="text-sm leading-tight">
+                      <span class="font-semibold">Programador Full Stack:</span>
+                      Análisis y desarrollo de sistemas de auditoría y control interno, para la evaluación y visibilidad de avances de construcción en tramos carreteros en colaboración con CAPUFE.
+                  </p>
+              </div>
+              <div class="mb-1">
+                  <h3 class="font-bold leading-tight">
+                      Banco de México
+                      <span class="text-sm font-normal">| Oct 2017 - Ene 2018</span>
+                  </h3>
+                  <p class="text-sm leading-tight">
+                      <span class="font-semibold">Analista de información:</span>
+                      Análisis y soporte a las diferentes entidades bancarias. Revisión de flujos de información de transacciones.
+                  </p>
+              </div>
+              <div class="mb-1">
+                  <h3 class="font-bold leading-tight">
+                      CNBV - Freelancer
+                      <span class="text-sm font-normal">| Jul 2016 - Sep 2016</span>
+                  </h3>
+                  <p class="text-sm leading-tight">
+                      <span class="font-semibold">Desarrollador Web:</span>
+                      Análisis de datos para la elaboración de reportes, mediante el desarrollo de sistemas propietarios.
+                  </p>
+              </div>
+              <div>
+                  <h3 class="font-bold leading-tight">
+                      Unisys de México, S.A. de C.V.
+                      <span class="text-sm font-normal">| Dic 2013 - Dic 2015</span>
+                  </h3>
+                  <p class="text-sm leading-tight">
+                      <span class="font-semibold">Student Technical:</span>
+                      Monitoreo de servidores y sistemas basados en COBOL, así como el desarrollo y prueba de nuevas tecnologías.
+                  </p>
+              </div>
+          </section>
         </div>
         <!-- Columna derecha -->
         <div class="w-full md:w-2/5 p-4">
-            <section class="mb-3">
-                <h2 class="mb-2 text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">Formación Académica</h2>
-                <p class="mb-1 text-sm leading-tight">
-                    Universidad Nacional Autónoma de México <br> Facultad de Ingeniería <br> Licenciatura (2008 – 2015)
-                </p>
-            </section>
-            <section class="mb-3">
-                <h2 class="mb-2 text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">Habilidades</h2>
-                <h3 class="font-bold text-sm">Front-End</h3>
-                <ul class="list-disc list-inside text-sm leading-tight">
-                    <li>HTML5, CSS3, JavaScript</li>
-                    <li>jQuery, VueJS, Astro</li> 
-                    <li>TailwindCSS, Bootstrap, Otros</li>
-                    <li>Nuxt</li>
-                </ul>
-                <h3 class="font-bold text-sm">Back-End</h3>
-                <ul class="list-disc list-inside text-sm leading-tight">
-                    <li>PHP, Laravel, Codeigniter, Cake, Lumen</li>
-                    <li>Python</li>
-                </ul>
-                <h3 class="font-bold text-sm">Base de Datos</h3>
-                <ul class="mb-1 list-disc list-inside text-sm leading-tight">
-                    <li>MySQL, MariaDB</li>
-                </ul>
-                <h3 class="font-bold text-sm">Control de Versiones</h3>
-                <ul class="mb-1 list-disc list-inside text-sm leading-tight">
-                    <li>Git, GitHub</li>
-                </ul>
-                <h3 class="font-bold text-sm">Plataformas de pruebas</h3>
-                <ul class="mb-1 list-disc list-inside text-sm leading-tight">
-                    <li>Postman</li>
-                </ul>
-                <h3 class="font-bold text-sm">Sistemas Operativos</h3>
-                <ul class="mb-1 list-disc list-inside text-sm leading-tight">
-                    <li>Windows, Linux</li>
-                </ul>
-                <h3 class="font-bold text-sm">Computo en la nube</h3>
-                <ul class="mb-1 list-disc list-inside text-sm leading-tight">
-                    <li>Azure, Firebase</li>
-                </ul>
-                <h3 class="font-bold text-sm">LMS</h3>
-                <ul class="mb-1 list-disc list-inside text-sm leading-tight">
-                    <li>Moodle</li>
-                </ul>
-            </section>
-            <section class="mb-3">
-                <h2 class="mb-2 text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">Repositorio</h2>
-                <ul class="list-disc list-inside text-sm leading-tight">
-                    <li>https://github.com/cr-gcc</li>
-                </ul>
-            </section>
-            <section class="mb-3">
-                <h2 class="mb-2 text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">Certificados</h2>
-                <ul class="list-disc list-inside text-sm leading-tight">
-                    <li>SCRUMStudy - Accreditation Body for Scrum Agile 2020</li>
-                </ul>
-            </section>
-            <section class="mb-3">
-                <h2 class="mb-2 text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">Información adicional</h2>
-                <ul class="list-disc list-inside text-sm leading-tight">
-                    <li>Fecha de nacimiento: 26-07-1990</li>
-                    <li>Inglés: B1</li>
-                </ul>
-            </section>
+          <section class="mb-3">
+              <h2 class="mb-2 text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">Formación Académica</h2>
+              <p class="mb-1 text-sm leading-tight">
+                  Universidad Nacional Autónoma de México <br> Facultad de Ingeniería <br> Licenciatura (2008 – 2015)
+              </p>
+          </section>
+          <section class="mb-3">
+              <h2 class="mb-2 text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">Habilidades</h2>
+              <h3 class="font-bold text-sm">Front-End</h3>
+              <ul class="list-disc list-inside text-sm leading-tight">
+                  <li>HTML5, CSS3, JavaScript</li>
+                  <li>Astro, Vue, Vite</li> 
+                  <li>TailwindCSS, Bootstrap, Vuetify, etc.</li>
+              </ul>
+              <h3 class="font-bold text-sm">Back-End</h3>
+              <ul class="list-disc list-inside text-sm leading-tight">
+                  <li>PHP, Laravel, Codeigniter, Cake, Lumen</li>
+                  <li>Python, Flask</li>
+              </ul>
+              <h3 class="font-bold text-sm">Base de Datos</h3>
+              <ul class="mb-1 list-disc list-inside text-sm leading-tight">
+                  <li>MySQL, MariaDB</li>
+                  <li>Mongo</li>
+              </ul>
+              <h3 class="font-bold text-sm">Control de Versiones</h3>
+              <ul class="mb-1 list-disc list-inside text-sm leading-tight">
+                  <li>Git, GitHub</li>
+              </ul>
+              <h3 class="font-bold text-sm">Gestion de Proyectos/Tareas</h3>
+              <ul class="mb-1 list-disc list-inside text-sm leading-tight">
+                  <li>Jira</li>
+                  <li>Trello</li>
+              </ul>
+              <h3 class="font-bold text-sm">Plataformas de Pruebas</h3>
+              <ul class="mb-1 list-disc list-inside text-sm leading-tight">
+                  <li>Postman</li>
+              </ul>
+              <h3 class="font-bold text-sm">API's</h3>
+              <ul class="mb-1 list-disc list-inside text-sm leading-tight">
+                  <li>GoogleMaps</li>
+                  <li>GraphHopper</li>
+                  <li>Twilio</li>
+              </ul>
+              <h3 class="font-bold text-sm">Sistemas Operativos</h3>
+              <ul class="mb-1 list-disc list-inside text-sm leading-tight">
+                  <li>Windows, Linux</li>
+              </ul>
+              <h3 class="font-bold text-sm">LMS</h3>
+              <ul class="mb-1 list-disc list-inside text-sm leading-tight">
+                  <li>Moodle</li>
+              </ul>
+          </section>
+          <section class="mb-3">
+              <h2 class="mb-2 text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">Repositorio</h2>
+              <ul class="list-disc list-inside text-sm leading-tight">
+                  <li>https://github.com/cr-gcc</li>
+              </ul>
+          </section>
+          <section class="mb-3">
+              <h2 class="mb-2 text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">Certificados</h2>
+              <ul class="list-disc list-inside text-sm leading-tight">
+                  <li>SCRUMStudy - Accreditation Body for Scrum Agile 2020</li>
+              </ul>
+          </section>
+          <section class="mb-3">
+              <h2 class="mb-2 text-lg font-semibold border-b-2 border-gray-400 uppercase tracking-widest">Información adicional</h2>
+              <ul class="list-disc list-inside text-sm leading-tight">
+                  <li>Fecha de nacimiento: 26-07-1990</li>
+                  <li>Inglés: B1</li>
+              </ul>
+          </section>
         </div>
     </div>
 </div>
